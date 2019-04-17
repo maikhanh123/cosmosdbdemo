1f467e44eb4e12374e7bddd8c6673e12010315c8
maikhanh123 <maikhanh123@gmail.com>
2019-04-18 05:40:55 +0700

    first commit

---
+++ PsHelloAzure/Services/CourseStore.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Documents.Client;
+using PsHelloAzure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PsHelloAzure.Services
+{
+    public class CourseStore
+    {
+        private DocumentClient client;
+        private Uri coursesLink;
+
+        public CourseStore()
+        {
+            var uri = new Uri("https://cosmosdemodb.documents.azure.com:443//");
+            var key = "7Sm44zcbFt9RyKhBfpauZXrZBYxC5xyS9cmlHOWQ4XKh2g2LvDEv9a2ZLIk8hxyKbfiS65pRB9XObBHWTIksRw==";
+            client = new DocumentClient(uri, key);
+            coursesLink = UriFactory.CreateDocumentCollectionUri("cosmosdbdemo", "courses");
+        }
+
+        public async Task InsertCourses(IEnumerable<Course> courses)
+        {
+            foreach(var course in courses)
+            {
+                await client.CreateDocumentAsync(coursesLink, course);
+            }
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            var option = new FeedOptions { EnableCrossPartitionQuery = true };
+            var courses = client.CreateDocumentQuery<Course>(coursesLink, option)
+                                .OrderBy(c => c.Title);
+
+            return courses;
+        }
+    }
+}
